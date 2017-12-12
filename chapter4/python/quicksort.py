b833c98a8ba490eafa4fa34b1bf251dc524d00ab
Bejoy Mathew <bejoygm@gmail.com>
2017-12-12 12:21:30 +0530

    quicksort in elixir and python

--- chapter4/python/quicksort.py
+++ chapter4/python/quicksort.py
@@ -1,7 +1,7 @@
 def quicksort(arr):
-    if len(arr) < 1:
+    if len(arr) < 2:
         return arr
     pivot = arr[0]
-    lesser = [item for item in arr if item <= arr[1:]]
-    greater = [item for item in arr if item > arr[1:]]
+    lesser = [item for item in arr[1:] if item <= pivot]
+    greater = [item for item in arr[1:] if item > pivot]
     return quicksort(lesser) + [pivot] + quicksort(greater)
